271d2f7935d9b443855408735d0b1933fb3fbf5d
STYLE.CS <styletime2609@gmail.com>
2026-03-11 17:42:53 +0800

    news
    added inventory

---
+++ Game top-Down/Assets/UI/UIScript/ItemDragHandler.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform originalParent;
+    CanvasGroup canvasGroup;
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        if (canvasGroup == null)
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+
+        transform.SetParent(transform.root);
+
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+
+        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+        Slot originalSlot = originalParent?.GetComponent<Slot>();
+
+        if (dropSlot != null)
+        {
+            if (dropSlot.currentItem != null && originalSlot != null)
+            {
+                GameObject swappedItem = dropSlot.currentItem;
+                swappedItem.transform.SetParent(originalSlot.transform);
+                swappedItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                originalSlot.currentItem = swappedItem;
+            }
+            else if (originalSlot != null)
+            {
+                originalSlot.currentItem = null;
+            }
+
+            transform.SetParent(dropSlot.transform);
+            dropSlot.currentItem = gameObject;
+        }
+        else
+        {
+            transform.SetParent(originalParent);
+        }
+
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+}
