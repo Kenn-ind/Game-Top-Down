5f9dc5e024198f320f22d0f465d4a9842a1773fe
Kaindra <indracapejb@gmail.com>
2026-04-05 22:34:12 +0800

    added death anim for enemys

--- Game top-Down/Assets/Enemys/EnemyScripts/Enemy.cs
+++ Game top-Down/Assets/Enemys/EnemyScripts/Enemy.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 public class Enemy : BaseEnemy
 {
     public float flashTime = 0.1f;
-
     private SpriteRenderer spriteRenderer;
     private Color originalColor;
 
     protected override void Start()
     {
         base.Start();
-
         spriteRenderer = GetComponent<SpriteRenderer>();
         originalColor = spriteRenderer.color;
     }
 
     public override void TakeDamage(int damage, Vector2 knockbackDir, bool applyKnockback = true)
     {
+        if (isDead) return; // Jangan flash jika sudah mati
+
         StartCoroutine(FlashRed());
-
         Debug.Log("Enemy kena hit");
-
         base.TakeDamage(damage, knockbackDir, applyKnockback);
     }
 
